837a4ac59aa5980903466328286df395eefff0d4
Will Jones <willjones2010@gmail.com>
2019-03-16 20:05:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,14 @@ def countdown (int)
   while int > 0
     puts "#{int} SECONDS"
     int -= 1
-    sleep(1)
 end
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECONDS"
+    int -= 1
+end
+return "HAPPY NEW YEAR!"
 end
